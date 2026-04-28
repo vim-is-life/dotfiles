26dbfdb925869868732bdd103e70fef58ecf11e9
vim-is-life <polar@arctic>
2026-04-27 21:55:47 -0500

    they removed pomodoro from doom, so i'll remove it from here i guess :(

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -153,7 +153,6 @@
        ;;ocaml             ; an objective camel
        (org
         +pandoc
-        +pomodoro
         +present)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
